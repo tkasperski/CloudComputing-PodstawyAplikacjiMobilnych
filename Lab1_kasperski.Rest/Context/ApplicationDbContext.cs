47d6d7f90f140127f6722a507f7c62ce77d91d85
Tomasz Kasperski <tkasperski98@gmail.com>
2021-03-09 09:29:03 +0100

    Context added

---
+++ Lab1_kasperski.Rest/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab1_kasperski.Rest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1_kasperski.Rest.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> People { get; set; }
+    }
+}
