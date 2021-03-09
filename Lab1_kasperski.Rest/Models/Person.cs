3f4f99ad02dce1acc6e9bf1520974ff7baefc29d
Tomasz Kasperski <tkasperski98@gmail.com>
2021-03-09 09:21:53 +0100

    Person class added

---
+++ Lab1_kasperski.Rest/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_kasperski.Rest.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+    }
+}
