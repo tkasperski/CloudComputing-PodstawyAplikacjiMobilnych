6ec2994c40826c5825f36c47fb5617a3c760720e
Tomasz Kasperski <tkasperski98@gmail.com>
2021-04-08 14:46:15 +0200

    Lab3 added

---
+++ Podstawy aplikacji mobilnych/Lab3/PeopleStorageApp/PeopleStorageApp.Web/LocalDataStorage.cs
@@ -0,0 +1,20 @@
+﻿using PeopleStorageApp.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleStorageApp.Web
+{
+    public class LocalDataStorage
+    {
+        private List<Person> people = new List<Person>();
+        public IReadOnlyCollection<Person> People => people;
+
+        public void AddPerson(Person p) 
+        {
+            p.Id = people.OrderByDescending(by => by.Id).FirstOrDefault()?.Id + 1 ?? 1;
+            people.Add(p);
+        }
+    }
+}
---
+++ Podstawy aplikacji mobilnych/Lab3/PeopleStorageApp/PeopleStorageApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using PeopleStorageApp.DataContracts;
+using Xamarin.Forms;
+
+namespace PeopleStorageApp
+{
+    public partial class App : Application
+    {
+
+        private const string API_URL = "http://localhost:55550/api";
+        public App()
+        {
+            var client = RestEase.RestClient.For<IPeopleClient>(API_URL);
+
+            InitializeComponent();
+
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
